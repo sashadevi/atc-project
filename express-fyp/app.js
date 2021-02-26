48975afd09cc4566226e1506eab86cf0f3504191
sashadevi <sashadevi@me.com>
2021-02-26 14:38:56 +0000

    install packages

--- express-fyp/app.js
+++ express-fyp/app.js
@@ -6,13 +6,12 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var transcriptionRouter = require('./routes/transcribe');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -22,7 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/transcribe', transcriptionRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
