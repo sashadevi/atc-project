af5b4ee85a4ba7a0e54c3c11e0ac6c01ffaa378d
sashadevi <sashadevi@me.com>
2021-03-03 14:07:55 +0000

    display text on screen

--- express-fyp/routes/transcribe.js
+++ express-fyp/routes/transcribe.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
+const speech = require('@google-cloud/speech');
+
+async function speechToText() {
+  const client = new speech.SpeechClient();
+  const filename = './public/resources/Atc02.mp3';
+  const encoding = 'MP3';
+  const sampleRateHertz = 16000;
+  const languageCode = 'en-UK';
+
+  const config = {
+    encoding: encoding,
+    sampleRateHertz: sampleRateHertz,
+    languageCode: languageCode,
+  };
+
+  const audio = {
+    content: fs.readFileSync(filename).toString('base64'),
+  };
+
+  const request = {
+    config: config,
+    audio: audio,
+  };
+
+  const [response] = await client.recognize(request);
+  const transcription = response.results.map(result => result.alternatives[0].transcript).join('\n');
+  console.log('Transcription', transcription);
+  router.get('/', function(req, res, next) {
+    res.render('Transcription: ', { title: 'ATC', transcription });
+  });
+}
+
+speechToText();
 
 
-router.get('/', function(req, res, next) {
-  res.render('transcription', { title: 'ATC' });
-});
 
 module.exports = router;
