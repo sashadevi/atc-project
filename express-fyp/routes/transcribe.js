dc33d533dfe579810b001c31e32dc6801f3f7339
sashadevi <sashadevi@me.com>
2021-03-19 10:44:11 +0000

    redesign the web page to make it dynamic

--- express-fyp/routes/transcribe.js
+++ express-fyp/routes/transcribe.js
@@ -5,15 +5,17 @@ const speech = require('@google-cloud/speech');
 
 async function speechToText() {
   const client = new speech.SpeechClient();
-  const filename = './public/resources/atc03.mp3';
-  const encoding = 'MP3';
-  const sampleRateHertz = 48000;
+  const filename = './public/resources/Animals.flac';
+  const encoding = 'FLAC';
+  const sampleRateHertz = 44100;
   const languageCode = 'en-UK';
 
   const config = {
     encoding: encoding,
     sampleRateHertz: sampleRateHertz,
     languageCode: languageCode,
+    audioChannelCount: 2,
+    enableSeparateRecognitionPerChannel: true
   };
 
   const audio = {
@@ -26,14 +28,47 @@ async function speechToText() {
   };
 
   const [response] = await client.recognize(request);
-  const transcription = response.results.map(result => result.alternatives[0].transcript).join('\n');
+  const transcription = response.results.map(result => result.alternatives[0].transcript);
   const confidence = response.results
   .map(result => result.alternatives[0].confidence)
   .join('\n');
+
+  var speechResults = [];
+  speechResults.push(transcription);
+  
+  const channel = response.results
+  .map(
+    result =>
+      ` Channel Tag: ${result.channelTag} ${result.alternatives[0].transcript}`
+  );
+
+  var color = "";
+
+  if (confidence > 0.7) {
+    color="green";
+  } else if (confidence > 0.5 && confidence < 0.7 ) {
+    color="orange";
+  } else if (confidence < 0.5) {
+    color="red";
+  }
+
+  var position="";
+  var icon="";
+  // for(var i=0; i < transcription.length; i ++) {
+  //   if (transcription.indexOf(i) == 1) {
+  //     position="left";
+  //   } else if (transcription.indexOf(i) == 0) {
+  //     position="right";
+  //   }
+  // }
+
+
+
   console.log('Transcription', transcription);
   console.log('Confidence', confidence);
+  console.log(channel);
   router.get('/', function(req, res, next) {
-    res.render('transcription', { title: 'ATC', transcription });
+    res.render('transcription', { title: 'ATC', transcription, color, position, icon });
   });
 }
 
