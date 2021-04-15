4f628564e46fef4801bf7791fc59c391110d2c93
sashadevi <sashadevi@me.com>
2021-04-15 10:07:53 +0100

    change file structure

--- express-fyp/src/config/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => {
-        console.log("DB Connected!")
-    }).catch((err) => console.log(err));
